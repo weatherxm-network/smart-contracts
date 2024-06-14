922dce599c4c423b821a41e64bc4fcc2e8984acc
NikosFotiadis <nikosfotiadis0@gmail.com>
2024-06-14 16:09:18 +0300

    chore: lint fixes

--- src/0.8.25/interfaces/IAlphaDeal.sol
+++ src/0.8.25/interfaces/IAlphaDeal.sol
@@ -4,54 +4,53 @@ pragma solidity 0.8.25;
 import { IERC721A } from "lib/erc721a/contracts/interfaces/IERC721A.sol";
 
 interface IAlphaDeal is IERC721A {
+  /* ========== ERRORS ========== */
+  error MetadataFrozen();
 
-    /* ========== ERRORS ========== */
-    error MetadataFrozen();
+  /* ========== EVENTS ========== */
+  event BatchMetadataUpdate(uint256 _fromTokenId, uint256 _toTokenId);
+  event MetadataFreeze();
+  event ContractURIUpdated();
 
-    /* ========== EVENTS ========== */
-    event BatchMetadataUpdate(uint256 _fromTokenId, uint256 _toTokenId);
-    event MetadataFreeze();
-    event ContractURIUpdated();
+  /* ========== ADMIN FUNCTIONS ========== */
 
-    /* ========== ADMIN FUNCTIONS ========== */
+  /**
+   * @notice Update the base URI
+   * @dev Update the base URI. Only callbale by the owner
+   * @param _baseURI The new base URI
+   */
+  function setBaseURI(string memory _baseURI) external;
+  /**
+   * @notice Update the contract URI
+   * @dev Update the contract URI. Only callbale by the owner
+   * @param _contractURI The new contract URI
+   */
+  function setContractURI(string memory _contractURI) external;
 
-    /**
-     * @notice Update the base URI
-     * @dev Update the base URI. Only callbale by the owner
-     * @param _baseURI The new base URI
-     */
-    function setBaseURI(string memory _baseURI) external;
-    /**
-     * @notice Update the contract URI
-     * @dev Update the contract URI. Only callbale by the owner
-     * @param _contractURI The new contract URI
-     */
-    function setContractURI(string memory _contractURI) external;
+  /**
+   * @notice Freeze metadata
+   * @dev Freeze metadata and prevert future base URI changes. Only callable by the owner
+   */
+  function freezeMetadata() external;
 
-    /**
-     * @notice Freeze metadata
-     * @dev Freeze metadata and prevert future base URI changes. Only callable by the owner
-     */
-    function freezeMetadata() external;
+  /* ========== USER FUNCTIONS ========== */
 
-    /* ========== USER FUNCTIONS ========== */
+  /**
+   * @notice Return the URI for a token
+   * @dev Return the URI for a token. Reverts if the token does not exist
+   * @param tokenId The token for which to return the URI
+   */
+  function tokenURI(uint256 tokenId) external view returns (string memory);
 
-    /**
-     * @notice Return the URI for a token
-     * @dev Return the URI for a token. Reverts if the token does not exist
-     * @param tokenId The token for which to return the URI
-     */
-    function tokenURI(uint256 tokenId) external view returns(string memory);
+  /**
+   * @notice Return the URI with the contract metadata
+   * @dev Return the URI with the contract metadata
+   */
+  function contractURI() external view returns (string memory);
 
-    /**
-     * @notice Return the URI with the contract metadata
-     * @dev Return the URI with the contract metadata
-     */
-    function contractURI() external view returns(string memory);
-
-    /**
-     * @notice Returns whether the token metadata are frozen or not
-     * @dev Returns true if the token metadata are frozen and false if they are not
-     */
-    function isMetadataFrozen() external view returns(bool);
+  /**
+   * @notice Returns whether the token metadata are frozen or not
+   * @dev Returns true if the token metadata are frozen and false if they are not
+   */
+  function isMetadataFrozen() external view returns (bool);
 }
